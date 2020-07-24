166a0f5f2ffdb7d6f0a10da24f3a1349c751e847
junwoochoi <cjw4690@gmail.com>
2020-07-24 11:00:07 +0900

    Discard Server, Handler 작성

---
+++ src/main/java/server/handler/DiscardServerHandler.java
@@ -0,0 +1,17 @@
+package server.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //Do nothing
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
